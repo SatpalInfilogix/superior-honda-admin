70c2c632ca72158fe5714bc7bf1466959240ac03
Satpal Singh Sekhon <satpalsekkhon01@gmail.com>
2024-05-03 10:06:25 +0530

    Updated input text component

--- resources/views/components/input-text.blade.php
+++ resources/views/components/input-text.blade.php
@@ -1,6 +1,23 @@
+@isset($attributes['label'])
 <label for="{{ $attributes['name'] }}">{{ $attributes['label'] }}</label>
-<input type="text" {{ $attributes->merge(['class' => 'form-control']) }} id="{{ $attributes['name'] }}" />
+@endisset
+
+@unset($attributes['label'])
+
+@php
+    $errorClass = '';
+@endphp
+
+@if($errors->has($attributes['name']))
+    @php
+        $errorClass = 'form-control-danger'
+    @endphp
+@endif
+
+<input type="text" id="{{ $attributes['name'] }}" autocomplete="false"
+    {{ $attributes->merge(['class' => 'form-control '.$errorClass]) }} 
+/>
 
 @error($attributes['name'])
-    <span class="text-danger">{{ $message }}</span>
+    <span class="text-danger f-12">{{ $message }}</span>
 @enderror
