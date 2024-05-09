7b8a51366d4c8510d12635086764db0227cdbde8
Satpal Singh Sekhon <satpalsekkhon01@gmail.com>
2024-05-09 13:34:23 +0530

    Fixed seed issue

--- database/seeders/VehicleTypeSeeder.php
+++ database/seeders/VehicleTypeSeeder.php
@@ -14,30 +14,30 @@ class VehicleTypeSeeder extends Seeder
     public function run(): void
     {
         // Bike Types
-        VehicleType::create(['category_id ' => '1', 'vehicle_type' => 'Cruisers']);
-        VehicleType::create(['category_id ' => '1', 'vehicle_type' => 'Sportbikes']);
-        VehicleType::create(['category_id ' => '1', 'vehicle_type' => 'Standard & Naked']);
-        VehicleType::create(['category_id ' => '1', 'vehicle_type' => 'Adventure (ADV)']);
-        VehicleType::create(['category_id ' => '1', 'vehicle_type' => 'Dual Sports & Enduros']);
-        VehicleType::create(['category_id ' => '1', 'vehicle_type' => 'Dirtbikes']);
-        VehicleType::create(['category_id ' => '1', 'vehicle_type' => 'Electric']);
-        VehicleType::create(['category_id ' => '1', 'vehicle_type' => 'Choppers']);
-        VehicleType::create(['category_id ' => '1', 'vehicle_type' => 'Touring']);
-        VehicleType::create(['category_id ' => '1', 'vehicle_type' => 'Sport Touring']);
-        VehicleType::create(['category_id ' => '1', 'vehicle_type' => 'Vintage & Customs']);
-        VehicleType::create(['category_id ' => '1', 'vehicle_type' => 'Modern Classics']);
-        VehicleType::create(['category_id ' => '1', 'vehicle_type' => 'Commuters & Minis']);
+        VehicleType::create(['category_id' => '1', 'vehicle_type' => 'Cruisers']);
+        VehicleType::create(['category_id' => '1', 'vehicle_type' => 'Sportbikes']);
+        VehicleType::create(['category_id' => '1', 'vehicle_type' => 'Standard & Naked']);
+        VehicleType::create(['category_id' => '1', 'vehicle_type' => 'Adventure (ADV)']);
+        VehicleType::create(['category_id' => '1', 'vehicle_type' => 'Dual Sports & Enduros']);
+        VehicleType::create(['category_id' => '1', 'vehicle_type' => 'Dirtbikes']);
+        VehicleType::create(['category_id' => '1', 'vehicle_type' => 'Electric']);
+        VehicleType::create(['category_id' => '1', 'vehicle_type' => 'Choppers']);
+        VehicleType::create(['category_id' => '1', 'vehicle_type' => 'Touring']);
+        VehicleType::create(['category_id' => '1', 'vehicle_type' => 'Sport Touring']);
+        VehicleType::create(['category_id' => '1', 'vehicle_type' => 'Vintage & Customs']);
+        VehicleType::create(['category_id' => '1', 'vehicle_type' => 'Modern Classics']);
+        VehicleType::create(['category_id' => '1', 'vehicle_type' => 'Commuters & Minis']);
 
         // Car Types
-        VehicleType::create(['category_id ' => '2', 'vehicle_type' => 'SUV']);
-        VehicleType::create(['category_id ' => '2', 'vehicle_type' => 'Hatchback']);
-        VehicleType::create(['category_id ' => '2', 'vehicle_type' => 'Crossover']);
-        VehicleType::create(['category_id ' => '2', 'vehicle_type' => 'Convertible']);
-        VehicleType::create(['category_id ' => '2', 'vehicle_type' => 'Sedan']);
-        VehicleType::create(['category_id ' => '2', 'vehicle_type' => 'Sports Car']);
-        VehicleType::create(['category_id ' => '2', 'vehicle_type' => 'Coupe']);
-        VehicleType::create(['category_id ' => '2', 'vehicle_type' => 'Minivan']);
-        VehicleType::create(['category_id ' => '2', 'vehicle_type' => 'Station Wagon']);
-        VehicleType::create(['category_id ' => '2', 'vehicle_type' => 'Pickup Truck']);
+        VehicleType::create(['category_id' => '2', 'vehicle_type' => 'SUV']);
+        VehicleType::create(['category_id' => '2', 'vehicle_type' => 'Hatchback']);
+        VehicleType::create(['category_id' => '2', 'vehicle_type' => 'Crossover']);
+        VehicleType::create(['category_id' => '2', 'vehicle_type' => 'Convertible']);
+        VehicleType::create(['category_id' => '2', 'vehicle_type' => 'Sedan']);
+        VehicleType::create(['category_id' => '2', 'vehicle_type' => 'Sports Car']);
+        VehicleType::create(['category_id' => '2', 'vehicle_type' => 'Coupe']);
+        VehicleType::create(['category_id' => '2', 'vehicle_type' => 'Minivan']);
+        VehicleType::create(['category_id' => '2', 'vehicle_type' => 'Station Wagon']);
+        VehicleType::create(['category_id' => '2', 'vehicle_type' => 'Pickup Truck']);
     }
 }
