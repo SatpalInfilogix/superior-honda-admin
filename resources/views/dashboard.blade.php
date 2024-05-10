9e0f397cf6f4c1c69263468f43505f308a0d7bc6
Satpal Singh Sekhon <satpalsekkhon01@gmail.com>
2024-05-10 16:59:46 +0530

    Added dashboard widgets

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,30 +7,83 @@
                 <div class="page-body">
 
                     <div class="row">
-                        <div class="col-sm-12">
-                            <div class="card">
-                                <div class="card-header">
-                                    <h5>Dashboard</h5>
-                                </div>
-                                <div class="card-block">
-                                    <p>
-                                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                                        sed do eiusmod tempor incididunt ut labore et dolore magna
-                                        aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                                        ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                                        Duis aute irure dolor
-                                        in reprehenderit in voluptate velit esse cillum dolore eu
-                                        fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                                        proident, sunt in culpa qui officia deserunt mollit anim id
-                                        est laborum."
-                                    </p>
+                        <div class="col-xl-3 col-md-6">
+                            <div class="card prod-p-card card-red">
+                                <div class="card-body">
+                                    <div class="row align-items-center m-b-30">
+                                        <div class="col">
+                                            <h6 class="m-b-5 text-white">Total Service Done</h6>
+                                            <h3 class="m-b-0 f-w-700 text-white">150</h3>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="far fa-calendar-check text-c-red f-18"></i>
+                                        </div>
+                                    </div>
+                                    <p class="m-b-0 text-white"><span class="label label-danger m-r-10">+11%</span>From
+                                        Previous Month</p>
                                 </div>
                             </div>
                         </div>
-                    </div>
 
+                        <div class="col-xl-3 col-md-6">
+                            <div class="card prod-p-card card-blue">
+                                <div class="card-body">
+                                    <div class="row align-items-center m-b-30">
+                                        <div class="col">
+                                            <h6 class="m-b-5 text-white">Total Earning in this Month</h6>
+                                            <h3 class="m-b-0 f-w-700 text-white">$15,830</h3>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="fas fa-dollar-sign text-c-blue f-18"></i>
+                                        </div>
+                                    </div>
+                                    <p class="m-b-0 text-white"><span class="label label-primary m-r-10">+12%</span>From
+                                        Previous Month</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-md-6">
+                            <div class="card prod-p-card card-green">
+                                <div class="card-body">
+                                    <div class="row align-items-center m-b-30">
+                                        <div class="col">
+                                            <h6 class="m-b-5 text-white">Total Orders Processed</h6>
+                                            <h3 class="m-b-0 f-w-700 text-white">96</h3>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="far fa-calendar-check text-c-green f-18"></i>
+                                        </div>
+                                    </div>
+                                    <p class="m-b-0 text-white"><span class="label label-success m-r-10">+52%</span>From
+                                        Previous Month</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-md-6">
+                            <div class="card prod-p-card card-yellow">
+                                <div class="card-body">
+                                    <div class="row align-items-center m-b-30">
+                                        <div class="col">
+                                            <h6 class="m-b-5 text-white">Total Orders in Queue</h6>
+                                            <h3 class="m-b-0 f-w-700 text-white">16</h3>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="fas fa-tags text-c-yellow f-18"></i>
+                                        </div>
+                                    </div>
+                                    <p class="m-b-0 text-white"><span class="label label-warning m-r-10">+52%</span>From
+                                        Previous Month</p>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+@endsection
+
+@section('head')
+    <link rel="stylesheet" href="{{ asset('assets/css/widget.css') }}">
 @endsection
