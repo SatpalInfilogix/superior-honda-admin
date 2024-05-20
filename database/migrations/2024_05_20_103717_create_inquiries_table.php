984117187d5eb783dc399f89c91fe6d2bb3c6299
ashaInfilogix <ashainfilogix@gmail.com>
2024-05-20 17:05:29 +0530

    change

--- database/migrations/2024_05_20_103717_create_inquiries_table.php
+++ database/migrations/2024_05_20_103717_create_inquiries_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('inquiries', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('cus_id');
             $table->string('name');
             $table->date('date')->nullable();
             $table->string('mileage')->nullable();
