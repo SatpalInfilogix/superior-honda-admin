6a60e3157ea59233fd997bf143255fa76aa61e4e
VishalInfilogix <vishalInfilogix@gmail.com>
2025-03-18 11:16:46 +0530

    new seeder for locations and branches added

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -18,49 +18,56 @@ class UserSeeder extends Seeder
             'first_name' => 'Super',
             'last_name' => 'Admin',
             'email' => 'superadmin@gmail.com',
-            'password' => Hash::make('123456')
+            'password' => Hash::make('123456'),
+            'branch_id' => 1
         ])->assignRole('Super Admin');
         
         User::create([
             'first_name' => 'Admin',
             'last_name' => 'Test',
             'email' => 'admin@gmail.com',
-            'password' => Hash::make('123456')
+            'password' => Hash::make('123456'),
+            'branch_id' => 2
         ])->assignRole('Admin');
 
         User::create([
             'first_name' => 'Customer',
             'last_name' => 'Service',
             'email' => 'customerservice@gmail.com',
-            'password' => Hash::make('123456')
+            'password' => Hash::make('123456'),
+            'branch_id' => 2
         ])->assignRole('Customer Service');
 
         User::create([
             'first_name' => 'Accoutant',
             'last_name' => 'test',
             'email' => 'accoutant@gmail.com',
-            'password' => Hash::make('123456')
+            'password' => Hash::make('123456'),
+            'branch_id' => 2
         ])->assignRole('Accountant');
 
         User::create([
             'first_name' => 'Inspection',
             'last_name' => 'Manager',
             'email' => 'inspection.manager@gmail.com',
-            'password' => Hash::make('123456')
+            'password' => Hash::make('123456'),
+            'branch_id' => 2
         ])->assignRole('Inspection Manager');
 
         User::create([
             'first_name' => 'Technician',
             'last_name' => 'Test',
             'email' => 'technician@gmail.com',
-            'password' => Hash::make('123456')
+            'password' => Hash::make('123456'),
+            'branch_id' => 2
         ])->assignRole('Technician');
 
         User::create([
             'first_name' => 'Satpal',
             'last_name' => 'Singh',
             'email' => 'customer@gmail.com',
-            'password' => Hash::make('123456')
+            'password' => Hash::make('123456'),
+            'branch_id' => 2
         ])->assignRole('Customer');
     }
 }
